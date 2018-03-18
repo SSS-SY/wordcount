9c596b267b67e9c16063e00fe4975b42f8b77593
SSS-SY <shuyuetuo@gmail.com>
2018-03-18 22:42:26 +0800

    readme更改以及测试用例和测试结果

---
+++ test/test1.c
@@ -0,0 +1,16 @@
+askdbgasddasvdsf aswfe askdbsad dfffvfd
+//
+
+
+dasd
+//dsad
+
+dde
+
+#error
+fsd
+!#@$:
+3324
+w  er freds else if
+switch  +&^%^($)
+  case
---
+++ test/test2.c
@@ -0,0 +1,13 @@
+fdf
+342,reyu
+*&^%
+
+768//
+sdfgghj///httr
+register
+///
+rwefs
+da///das
+
+asdtr
+gd   rete  try,rtytgfh,ytu
