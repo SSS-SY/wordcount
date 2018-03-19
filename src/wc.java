0b4587d7660e4a5da64d2ef53c7f73e8c29dc196
SSS-SY <shuyuetuo@gmail.com>
2018-03-19 19:44:11 +0800

    修改了参数解析的逻辑

--- src/wc.java
+++ src/wc.java
@@ -6,74 +6,11 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 public class wc {
-    //参数设置
-    private static class Args {
-        boolean c = false;
-        boolean w = false;
-        boolean l = false;
-        boolean o = false;
-        boolean a = false;
-        boolean e = false;
-        boolean s =false;
-        String input=null;
-        String output=null;
-        String stop=null;
-
-        Args(String[] args) {
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].charAt(0) == '-' && args[i].length() < 3) {
-                    switch (args[i].charAt(1)) {
-                        case 't':
-                            this.c = true;
-                            break;
-                        case 'w':
-                            this.w = true;
-                            break;
-                        case 'l':
-                            this.l = true;
-                            break;
-                        case 'c':
-                            this.c = true;
-                            break;
-                        case 'o':
-                            this.o = true;
-                            i++;
-                            if (i < args.length) {
-                                this.output = args[i];
-                            } else {
-                                error("lacking path of out put file");
-                            }
-                            break;
-                        case 'a':
-                            this.a=true;
-                            break;
-                        case 's':
-                            this.s=true;
-                            break;
-                        case 'e':
-                            this.e=true;
-                            i++;
-                            if (i<args.length){
-                                this.stop = args[i];
-                            } else {
-                                error("lacking path of out put file");
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                } else {
-                    this.input = args[i];
-                }
-            }
-        }
-    }
-
     //stop words:
-    private static Set<String> stopWords(String stopfile){
-        Set<String> s=new HashSet<>();
+    private static Set<String> stopWords(String stopfile) {
+        Set<String> s = new HashSet<>();
         s.add("");
-        if (stopfile==null)
+        if (stopfile == null)
             return s;
         try {
             String[] words = getWords(stopfile);
@@ -87,11 +24,11 @@ public class wc {
     }
 
     //计算单词数
-    private static int wc(String contents,Set<String> stopLists) {
+    private static int wc(String contents, Set<String> stopLists) {
         int count = 0;
         String[] spStr = contents.split("[ ,]");
         for (String word : spStr) {
-            if(!stopLists.contains(word))
+            if (!stopLists.contains(word))
                 count++;
         }
         return count;
@@ -106,20 +43,18 @@ public class wc {
     }
 
     //计算行数
-    private static int[] cl(String[] contents){
+    private static int[] cl(String[] contents) {
         //code／blank/comments
-        int[] lines={0,0,0};
-        for (String line: contents){
-            if(line.trim().length()<=1)
+        int[] lines = {0, 0, 0};
+        for (String line : contents) {
+            if (line.trim().length() <= 1)
                 lines[1]++;
             else {
-                if (!line.trim().contains("//")){
+                if (!line.trim().contains("//")) {
                     lines[0]++;
-                }
-                else if(line.replaceAll(" ","").indexOf("//")>1){
+                } else if (line.replaceAll(" ", "").indexOf("//") > 1) {
                     lines[0]++;
-                }
-                else if(line.contains("//")){
+                } else if (line.contains("//")) {
                     lines[2]++;
                 }
             }
@@ -128,19 +63,18 @@ public class wc {
     }
 
     //按行获取文件内容
-    private static ArrayList<String> getLines(String path){
-        ArrayList<String> contents=new ArrayList<>();
-        Scanner scanner=null;
-        try{
-            File file=new File(path);
-            scanner=new Scanner(file);
-        }
-        catch (FileNotFoundException e){
-            error("cannot find file: "+path);
+    private static ArrayList<String> getLines(String path) {
+        ArrayList<String> contents = new ArrayList<>();
+        Scanner scanner = null;
+        try {
+            File file = new File(path);
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            error("cannot find file: " + path);
             e.printStackTrace();
         }
-        if(scanner!=null){
-            while(scanner.hasNextLine()){
+        if (scanner != null) {
+            while (scanner.hasNextLine()) {
                 contents.add(scanner.nextLine());
             }
             scanner.close();
@@ -154,19 +88,18 @@ public class wc {
     }
 
     //按字获取文件内容
-    private static String[] getWords(String path){
-        String[] res=null;
+    private static String[] getWords(String path) {
+        String[] res = null;
         try {
             InputStream is = new FileInputStream(path);
             BufferedReader in = new BufferedReader(new InputStreamReader(is));
             StringBuffer buffer = new StringBuffer();
             String line = "";
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 buffer.append(line);
             }
-            res= buffer.toString().split("[\n \t]]");
-        }
-        catch (Exception e){
+            res = buffer.toString().split("[\n \t]]");
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -174,60 +107,58 @@ public class wc {
     }
 
     //输出结果
-    private static void ptintResultToFile(String file,Args args,PrintWriter output){
-        ArrayList<String> lineContents=getLines(file);
-        String[] contents=lineContents.toArray(new String[lineContents.size()]);
-        if (args.c) {
+    private static void printResultToFile(String file, HashMap<Character, String> args, PrintWriter output) {
+        ArrayList<String> lineContents = getLines(file);
+        String[] contents = lineContents.toArray(new String[lineContents.size()]);
+        if (args.containsKey('c')) {
             output.printf("%s, 字符数: %d\n", file, cc(contents));
         }
-        if (args.w){
-            Set<String> stopwords=stopWords(args.stop);
-            int words=0;
-            for (String line:contents){
-                words+=wc(line,stopwords);
+        if (args.containsKey('w')) {
+            Set<String> stopwords = stopWords(args.get('e'));
+            int words = 0;
+            for (String line : contents) {
+                words += wc(line, stopwords);
             }
-            output.printf("%s，单词数: %d\n",file,words);
+            output.printf("%s，单词数: %d\n", file, words);
         }
-        if (args.l){
-            output.printf("%s，行数: %d\n",file,contents.length);
+        if (args.containsKey('l')) {
+            output.printf("%s，行数: %d\n", file, contents.length);
         }
-        if (args.a){
-            int[] lines=cl(contents);
-            output.printf("%s, 代码行／空行／注释行: %d/%d/%d\n",file,lines[0],lines[1],lines[2]);
+        if (args.containsKey('a')) {
+            int[] lines = cl(contents);
+            output.printf("%s, 代码行／空行／注释行: %d/%d/%d\n", file, lines[0], lines[1], lines[2]);
         }
     }
 
     //递归寻找文件
-    private static String[] findFiles(String path,boolean s){
+    private static String[] searchFiles(String path, boolean s) {
         File fp = new File(path);
         int idx = path.lastIndexOf('/');
         String dirpath, filename;
         ArrayList<String> sal = new ArrayList<>();
-        if(fp.isDirectory()) {
-            for(File f : fp.listFiles()) {
-                if(f.isFile()) {
+        if (fp.isDirectory()) {
+            for (File f : fp.listFiles()) {
+                if (f.isFile()) {
                     sal.add(f.getPath());
-                } else if(f.isDirectory() && s) {
-                    sal.addAll(Arrays.asList(findFiles(f.getPath(), s)));
+                } else if (f.isDirectory() && s) {
+                    sal.addAll(Arrays.asList(searchFiles(f.getPath(), s)));
                 }
             }
-        }
-        else {
-            if(idx > 0) {
-                dirpath = "./" + path.substring(0, idx+1);
-                filename = path.substring(idx+1, path.length());
-            }
-            else {
+        } else {
+            if (idx > 0) {
+                dirpath = "./" + path.substring(0, idx + 1);
+                filename = path.substring(idx + 1, path.length());
+            } else {
                 dirpath = "./";
                 filename = path;
             }
-            if(filename.contains("*")) {
+            if (filename.contains("*")) {
                 filename = filename.replaceAll("\\*", "\\\\w*");
             }
             File[] fs = (new File(dirpath)).listFiles();
             assert fs != null;
             for (File f : fs) {
-                if(f.getName().matches(filename) && f.isFile()) {
+                if (f.getName().matches(filename) && f.isFile()) {
                     sal.add(dirpath + f.getName());
                 }
             }
@@ -235,30 +166,61 @@ public class wc {
         return sal.toArray(new String[sal.size()]);
     }
 
-    public static void main(String[] args){
-        Args args1=new Args(args);
-        String output="result.txt";
-        if (args1.input==null){
+    //参数设置
+    private static HashMap<Character, String> parseArgs(String[] args) {
+        HashMap<Character, String> argsmap = new HashMap<Character, String>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].charAt(0) == '-' && args[i].length() < 3) {
+                switch (args[i].charAt(1)) {
+                    case 'o':
+                        i++;
+                        if (i < args.length) {
+                            argsmap.put(args[i].charAt(1), args[i]);
+                        } else {
+                            error("lacking path of out put file");
+                        }
+                        break;
+                    case 'e':
+                        i++;
+                        if (i < args.length) {
+                            argsmap.put(args[i].charAt(1), args[i]);
+                        } else {
+                            error("lacking path of out put file");
+                        }
+                        break;
+                    default:
+                        argsmap.put(args[i].charAt(1), "");
+                        break;
+                }
+            } else {
+                argsmap.put('i', args[i]);
+            }
+        }
+
+        return argsmap;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, String> argsmap = parseArgs(args);
+        String output = "result.txt";
+        if (!argsmap.containsKey('i')) {
             error("please input the name of inputFile");
             return;
         }
-        //修改输出文件
-        if (args1.o){
-            output=args1.output;
+        if (argsmap.containsKey('o')) {
+            output = argsmap.get('o');
         }
-        //创建输出文件
-        File file=new File(output);
-        try{
-            if (!file.exists()){
+        File file = new File(output);
+        try {
+            if (!file.exists()) {
                 file.createNewFile();
             }
-            //输出到文件
-            PrintWriter out=new PrintWriter(output);
-            String[] filLists=findFiles(args1.input,args1.s);
-            for(String f:filLists)
-                ptintResultToFile(f,args1,out);
+            PrintWriter out = new PrintWriter(output);
+            String[] fileList = searchFiles(argsmap.get('i'), argsmap.containsKey('s'));
+            for (String f : fileList)
+                printResultToFile(f, argsmap, out);
             out.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
